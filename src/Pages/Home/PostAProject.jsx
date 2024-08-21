a92dcba5befdda2a7f015eb430de597bf12a9e19
codewithkhalil <dareythefirst@gmail.com>
2024-08-21 14:47:09 +0100

    update site content

--- src/Pages/Home/PostAProject.jsx
+++ src/Pages/Home/PostAProject.jsx
@@ -4,7 +4,7 @@ const PostAProject = () => {
   return (
     <div className="w-full bg-gray-100 p-9 rounded-lg text-center">
       <h2 className=" text-2xl xl:text-3xl font-bold">Post a Project</h2>
-      <p className="text-sm xl:text-base my-6">Every day, 10,000+ clients visit our website. Connect with top talent by posting your projects today</p>
+      <p className="text-sm xl:text-base my-6">Build a tech team for your project, expand your network, and earn your TechTalent badge today.</p>
       <Button type="primary" block className='py-5 px-12 font-bold'>Post a Project</Button>
     </div>
   );
--- src/Pages/Home/TechExcellence.jsx
+++ src/Pages/Home/TechExcellence.jsx
@@ -2,15 +2,16 @@ import image from '../../assets/images/excellence.png'
 import Container from '../../components/Container';
 const TechExcellenceSection = () => {
   return (
-    <Container className="grid lg:grid-cols-2 gap-6 md:gap-10 lg:gap-12 p-8 bg-white 2xl:px-36">
-      <div className="w-full flex justify-center lg:justify-start mb-2 lg:mb-0">
+    <Container className="flex flex-col lg:flex-row gap-6 md:gap-10 lg:gap-12 p-8 bg-white 2xl:px-36">
+    {/* <Container className="grid lg:grid-cols-2 gap-6 md:gap-10 lg:gap-12 p-8 bg-white 2xl:px-36"> */}
+      <div className="lg:w-[50%] lg:h-[746px] flex justify-center mb-2 lg:mb-0 shrink-0 ">
         <img src={image} alt="Group of people" className="object-cover object-center rounded-3xl" />
       </div>
-      <div className=" w-full">
-        <h2 className="text-2xl xl:text-3xl font-bold mb-4">
+      <div className=" w-full flex flex-col gap-5 justify-between h-full lg:h-[746px]">
+        <h2 className="text-2xl xl:text-3xl font-bold">
           Achieve tech excellence at TechTalents City.
         </h2>
-        <ul className="space-y-6 mt-6 md:mt-12 xl:mt-20">
+        <ul className="space-y-6 ">
           <li>
             <div className="flex items-start">
               <div className="shrink-0 w-3 h-16 rounded-3xl bg-blue-600 mr-3"></div>
