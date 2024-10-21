24a8cc5a6adec22f4edb71e03a24db96be32084f
techtalentscity <techtalentscity@gmail.com>
2024-10-21 23:18:45 +0100

    Update Hero.jsx

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       <Container className="flex flex-col justify-center items-center h-screen pb-16 px-4 lg:px-20 xl:px-32">
         <div className='text-center max-w-3xl'>
           <p className="font-bold pt-12 sm:mt-0 text-4xl md:text-7xl"><span className='text-[#E8B300]'>Ascend</span> <span className='text-[#E73015]'>Achieve</span> <span className='text-[#0559A5]'>Advance</span></p>
-          <p className='py-16 text-sm'>Welcome to TechTalents City, where technology and talent converge to unleash your potential and create endless opportunities</p>
+          <p className='py-16'>Welcome to TechTalents City, where technology and talent converge to unleash your potential and create endless opportunities</p>
           <div className='w-full rounded-xl shadow-md bg-white/80 flex justify-between items-center py-4 px-5'>
               <p className='text-black/50'>Search Projects</p>
               <div className="bg-black w-10 h-10 rounded-full p-3 flex justify-center items-center">
