1789d616270eff052822b0086ebfcc343916627b
techtalentscity <techtalentscity@gmail.com>
2024-12-19 23:00:44 -0500

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Hero from "./Hero"
 import HireTalent from "./HireTalent"
 import HomeProjects from "./HomeProjects"
 import TechBadges from "./TechBadges"
+import event from "./event
 // import TechExcellenceSection from "./TechExcellence"
 import Testimonials from "./Testimonial"
 
