07a8cbcdac41004fababc14a0bef54d0d1aa574e
techtalentscity <techtalentscity@gmail.com>
2024-12-19 23:06:12 -0500

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,10 @@
-import Hero from "./Hero"
-import HireTalent from "./HireTalent"
-import HomeProjects from "./HomeProjects"
-import TechBadges from "./TechBadges"
-import event from "./event
-// import TechExcellenceSection from "./TechExcellence"
-import Testimonials from "./Testimonial"
+import Hero from "./Hero";
+import HireTalent from "./HireTalent";
+import HomeProjects from "./HomeProjects";
+import TechBadges from "./TechBadges";
+import event from "./event"; // Corrected missing closing quotation mark
+// import TechExcellenceSection from "./TechExcellence";
+import Testimonials from "./Testimonial";
 
 const Home = () => {
   return (
@@ -16,7 +16,7 @@ const Home = () => {
         <TechBadges />
         <Testimonials />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
