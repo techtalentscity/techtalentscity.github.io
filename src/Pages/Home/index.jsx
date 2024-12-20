9aacef37abf33d76f2619772263c5032131361bd
techtalentscity <techtalentscity@gmail.com>
2024-12-19 23:50:54 -0500

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,8 +2,9 @@ import Hero from "./Hero";
 import HireTalent from "./HireTalent";
 import HomeProjects from "./HomeProjects";
 import TechBadges from "./TechBadges";
-import Testimonials from "./Testimonial";
 import Events from "./event"; // Correct import for the Events component
+import Testimonials from "./Testimonial";
+
 
 const Home = () => {
   return (
@@ -11,8 +12,8 @@ const Home = () => {
         <Hero />
         <HomeProjects />
         <TechBadges />
-        <Testimonials />
         <Events /> {/* Correctly used Events component */}
+        <Testimonials />
     </div>
   );
 };
