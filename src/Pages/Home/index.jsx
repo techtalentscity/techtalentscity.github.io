77e7f9d13f439a660ccea8e5eacdedecb48732ab
techtalentscity <techtalentscity@gmail.com>
2025-01-21 14:55:28 -0500

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import Hero from "./Hero";
 import HireTalent from "./HireTalent";
 import HomeProjects from "./HomeProjects";
-import Events from "./event"; // Correct import for the Events component
 import TechBadges from "./TechBadges";
 import Testimonials from "./Testimonial";
 
@@ -11,7 +10,6 @@ const Home = () => {
     <div>
         <Hero />
         <HomeProjects />
-        <Events /> {/* Correctly used Events component */}
         <TechBadges />
         <Testimonials />
     </div>
