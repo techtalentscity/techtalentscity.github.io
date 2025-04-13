6dc28284c05b9c7fc74b863d560ed1effe66c454
techtalentscity <techtalentscity@gmail.com>
2025-04-13 18:56:40 -0400

    Update HomeProjects.jsx

--- src/Pages/Home/HomeProjects.jsx
+++ src/Pages/Home/HomeProjects.jsx
@@ -7,49 +7,33 @@ const HomeProjects = () => {
 
     const projects = [
         {
-          title: 'Augmented Reality (AR) Application Development',
-          skills: ['AR Developers', '3D Animators', 'UI Designer', 'Full Stack Developer'],
+          title: 'Building an AI-powered Agent for Customer Engagement',
+          skills: ['NLP Engineers', 'Machine Learning Developers', 'Frontend Developers', 'Backend Developers', 'UI/UX Designers', 'Cybersecurity Specialists'],
           logo: techtalent,
           verified: true,
-          premium: true,
-          price: '$500',
-        },
-        {
-          title: 'SmartCity Integration System',
-          skills: ['Full-stack Developers', 'IoT Specialists', 'Data Scientists', 'Cloud Architects'],
-          logo: techtalent,
-          verified: false,
           premium: false,
           price: 'Free',
         },
         {
-          title: 'Augmented Reality (AR) Application Development',
-          skills: ['AR Developers', '3D Animators', 'UI Designer', 'Full Stack Developer'],
+          title: 'Developing a Mobile App for Mental Health Support',
+          skills: ['Mobile App Developers', 'UI/UX Designers', 'Backend Developers', 'Data Security Specialists', 'Mental Health Researchers'],
           logo: techtalent,
           verified: true,
-          premium: true,
-          price: '$500',
-        },
-        {
-          title: 'SmartCity Integration System',
-          skills: ['Full-stack Developers', 'IoT Specialists', 'Data Scientists', 'Cloud Architects'],
-          logo: techtalent,
-          verified: false,
           premium: false,
           price: 'Free',
         },
-        // Add more project objects as needed
-      ];
-  return (
-    <Container className="flex flex-col-reverse lg:flex-row lg:items-start md:p-4 space-y-4 lg:space-y-0 lg:space-x-8 my-10 lg:my-[60px]">
-        <div className="lg:flex-grow">
-            <RecentProjects projects={projects} />
-        </div>
-        <div className="lg:w-1/4 flex-shrink-0 !mb-8">
-            <PostAProject />
-        </div>
-    </Container>
-  )
+    ];
+
+    return (
+      <Container className="flex flex-col-reverse lg:flex-row lg:items-start md:p-4 space-y-4 lg:space-y-0 lg:space-x-8 my-10 lg:my-[60px]">
+          <div className="lg:flex-grow">
+              <RecentProjects projects={projects} />
+          </div>
+          <div className="lg:w-1/4 flex-shrink-0 !mb-8">
+              <PostAProject />
+          </div>
+      </Container>
+    )
 }
 
-export default HomeProjects
+export default HomeProjects;
