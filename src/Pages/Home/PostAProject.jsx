a93d90f6989fdac428263df68e08b8e0038e3a00
techtalentscity <techtalentscity@gmail.com>
2025-04-12 23:45:53 -0400

    Update PostAProject.jsx

--- src/Pages/Home/PostAProject.jsx
+++ src/Pages/Home/PostAProject.jsx
@@ -1,11 +1,22 @@
 import { Button } from 'antd';
 
 const PostAProject = () => {
+  const handleClick = () => {
+    window.open('https://forms.gle/gvV1yXuGy8ZrYJWK9', '_blank');
+  };
+
   return (
     <div className="w-full bg-gray-100 p-9 rounded-lg text-center">
-      <h2 className=" text-2xl xl:text-3xl font-bold">Post a Project</h2>
+      <h2 className="text-2xl xl:text-3xl font-bold">Post a Project</h2>
       <p className="text-sm xl:text-base my-6">Share your ideas and collaborate with tech talent to bring your vision to life!</p>
-      <Button type="primary" block className='py-5 px-12 font-bold'>Post Your Project Now</Button>
+      <Button 
+        type="primary" 
+        block 
+        className="py-5 px-12 font-bold" 
+        onClick={handleClick}
+      >
+        Post Your Project Now
+      </Button>
     </div>
   );
 };
