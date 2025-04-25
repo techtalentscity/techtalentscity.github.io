6ef8f67a352992ba3af93f32f77b12d72deba545
techtalentscity <techtalentscity@gmail.com>
2025-04-25 12:50:13 -0400

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,15 +3,16 @@ import HireTalent from "./HireTalent";
 import HomeProjects from "./HomeProjects";
 import TechBadges from "./TechBadges";
 import Testimonials from "./Testimonial";
-
+import TechPathNavigator from "./TechPathNavigator"; // Importing the new component
 
 const Home = () => {
   return (
     <div>
-        <Hero />
-        <HomeProjects />
-        <TechBadges />
-        <Testimonials />
+      <Hero />
+      <HomeProjects />
+      <TechBadges />
+      <TechPathNavigator /> {/* Adding it to the page structure */}
+      <Testimonials />
     </div>
   );
 };
