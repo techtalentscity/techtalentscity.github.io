98a2d992996dd727efa810be4a9d37e79e9eb518
techtalentscity <techtalentscity@gmail.com>
2025-05-05 13:40:46 -0400

    Update application.jsx

--- src/Pages/Apply/application.jsx
+++ src/Pages/Apply/application.jsx
@@ -152,12 +152,12 @@ const Application = () => {
                 Next, <strong>select the badge role</strong> that best represents your expected contribution to the project. Badge roles reflect the area in which you'll be most active:
               </p>
               <ul className="list-disc pl-6 mb-3">
-                <li><strong>TechMO</strong> – Tech Mentors</li>
-                <li><strong>TechQA</strong> – Quality Testers</li>
+                <li><strong>TechPO</strong> – Product Owners</li>
+                <li><strong>TechQA</strong> – Quality Assurance/Testers</li>
+                <li><strong>TechLeads</strong> – Non-technical contributors</li>
+                <li><strong>TechArchs</strong> – Low-code/No-code developers</li>
+                <li><strong>TechGuard</strong> – Network and Cybersecurity</li>
                 <li><strong>TechDev</strong> – Coding Developers</li>
-                <li><strong>TechLeads</strong> – Non-Technical Professionals</li>
-                <li><strong>TechArchs</strong> – Low/No-Coding Developers</li>
-                <li><strong>TechGuard</strong> – Network and Cybersecurity professionals</li>
               </ul>
             </div>
             
