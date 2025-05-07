f6bd7c39cc928f998cbbbdd361b383c52ff75084
techtalentscity <techtalentscity@gmail.com>
2025-05-07 06:48:39 -0400

    Update AllProjects.jsx

--- src/Pages/Projects/AllProjects.jsx
+++ src/Pages/Projects/AllProjects.jsx
@@ -6,12 +6,17 @@ const AllProjects = ({projects}) => {
     <div className="w-full">
         <div className="space-y-4">
             {projects.map((project, index) => (
-            <ProjectCard key={index} project={project} />
+              <ProjectCard key={index} project={project} />
             ))}
         </div>
+
+        {/* 
         <div className="flex justify-center mt-8">
-            <Button ghost className="!text-[#737373] !border-[#737373] py-5 px-12 font-bold">Load more</Button>
-        </div>
+            <Button ghost className="!text-[#737373] !border-[#737373] py-5 px-12 font-bold">
+              Load more
+            </Button>
+        </div> 
+        */}
     </div>
   )
 }
